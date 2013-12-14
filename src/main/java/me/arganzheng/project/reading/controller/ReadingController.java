5bb1c66ac73c1737221c9cfff44dfc1ba869a4ef
arganzheng <arganzheng@gmail.com>
2013-12-14 11:31:40 +0800

    匹配根目录

--- src/main/java/me/arganzheng/project/reading/controller/ReadingController.java
+++ src/main/java/me/arganzheng/project/reading/controller/ReadingController.java
@@ -1,15 +1,12 @@
 package me.arganzheng.project.reading.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ReadingController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-        return "index";
-    }
+    // @RequestMapping(value = "/", method = RequestMethod.GET)
+    // public String index(Model model) {
+    // return "index";
+    // }
 }
