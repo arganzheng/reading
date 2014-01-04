6e444d8e3d1716d4fed4542578ec12d1d69fd7b8
arganzheng <arganzheng@gmail.com>
2014-01-04 17:20:38 +0800

    isbn detail页面

--- src/main/java/me/arganzheng/project/reading/controller/BookController.java
+++ src/main/java/me/arganzheng/project/reading/controller/BookController.java
@@ -11,11 +11,10 @@ import me.arganzheng.project.reading.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/book")
 public class BookController {
 
     @Autowired
@@ -27,11 +26,15 @@ public class BookController {
     private BookFacade  bookFacade;
 
     /**
-     * 根据 书名、作者、ISBN查询，默认是安装上传时间倒叙
+     * <pre>
+     * 根据 书名、作者、ISBN查询，默认是按照上架时间倒序。 
+     * TODO 迭代一只支持ISBN查询。
+     * 
+     * </pre>
      * 
      * @return
      */
-    @RequestMapping(value = "/**", method = RequestMethod.GET)
+    @RequestMapping("/book")
     public String search(BookPagingCriteria pagingCriteria, Model model) {
         Page<Book> books = Page.emptyPage();
         pagingCriteria.setIncludeOwnership(true);
@@ -41,5 +44,13 @@ public class BookController {
         model.addAttribute("pagingCriteria", pagingCriteria);
         model.addAttribute("books", books);
         return "list";
+    }
+
+    @RequestMapping(value = "/book/{isbn}")
+    public String detail(@PathVariable
+    String isbn, Model model) {
+        Book book = bookService.getBookByISBN(isbn, true);
+        model.addAttribute("book", book);
+        return "detail";
     }
 }
